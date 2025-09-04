6fc877ac84bdebb2e83ee4b15534b07fda8b0699
Hista <dtmdgus901@gmail.com>
2025-09-04 15:07:05 +0900

    feat: HTTP 정적 파일 응답

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -1,12 +1,18 @@
 package org.apache.coyote.http11;
 
 import com.techcourse.exception.UncheckedServletException;
+
 import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.URL;
+import java.nio.file.Files;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -24,19 +30,30 @@ public class Http11Processor implements Runnable, Processor {
         process(connection);
     }
 
+    private String getBody(String requestUri) throws IOException {
+        if (requestUri.equals("/")) {
+            return "Hello world!";
+        }
+
+        final URL resource = getClass().getClassLoader().getResource("static" + requestUri);
+        return new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+    }
+
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final var reader = new BufferedReader(new InputStreamReader(inputStream))) {
 
-            final var responseBody = "Hello world!";
+            String requestUri = reader.readLine().split(" ")[1];
+            final var responseBody = getBody(requestUri);
 
             final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+                "HTTP/1.1 200 OK ",
+                "Content-Type: text/html;charset=utf-8 ",
+                "Content-Length: " + responseBody.getBytes().length + " ",
+                "",
+                responseBody);
 
             outputStream.write(response.getBytes());
             outputStream.flush();
@@ -44,4 +61,5 @@ public class Http11Processor implements Runnable, Processor {
             log.error(e.getMessage(), e);
         }
     }
+
 }
